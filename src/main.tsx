02b05348532572d1b46b7e672ed1f288a99c3770
ngscode23 <stasvolohovish@gmail.com>
2025-08-19 16:50:49 +0200

    fix: ensure Tailwind pipeline and css import

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 // src/main.tsx
+import '@fontsource-variable/inter/index.css'; // <= важно: /index.css
+import './index.css';
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./index.css";
+
 
 const container = document.getElementById("root");
 if (!container) {
